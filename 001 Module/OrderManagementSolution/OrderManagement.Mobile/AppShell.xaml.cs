048ba2d518b7b0d583fc60e9f8de295dce7589a7
Said WAHID <said@teknologiate.com>
2026-06-24 11:52:25 +0100

    Create Mobile Projet

---
+++ 001 Module/OrderManagementSolution/OrderManagement.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagement.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
